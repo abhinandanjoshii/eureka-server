c7a42e95cf555181c3f1945dd5dadfa7c8b64fe2
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-21 22:13:03 +0530

    eureka server v0.0

---
+++ src/test/java/com/server/eureka_server_microservice_registry/EurekaServerMicroserviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.eureka_server_microservice_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMicroserviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
